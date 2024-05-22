d9d1056b07c7fb1d3e04969b082c681b8178bb65
Ogunlade Stephen O <73673531+honordevop@users.noreply.github.com>
2024-05-22 18:47:31 +0100

    Update ContactCard.jsx

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -8,7 +8,8 @@ const ContactCard = (props) => {
       <p className="text-xl font-bold ">{props.data.title} </p>
       <p className="text-lg">{props.data.desc}</p>
 
-      <Link
+      <Link target="_blank"
+      rel="noopener noreferrer"
         className="bg-blue-400 py-2 px-4 text-white rounded-md text-center"
         href={props.data.url}
       >
